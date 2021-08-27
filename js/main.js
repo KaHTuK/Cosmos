9bd64032d0116a62f3e5aff8f04fcf51ff5097dd
KaHTuK <88055736+KaHTuK@users.noreply.github.com>
2021-08-27 17:49:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(function () {
   $(".slider_box").slick({
     prevArrow:
-      '<img class="slider_arrow slider_arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
+      '<img class="slider_arrow slider_arrow-left" src="images/Arrow-left.svg" alt="arrow-left">',
     nextArrow:
-      '<img class="slider_arrow slider_arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
+      '<img class="slider_arrow slider_arrow-right" src="images/Arrow-right.svg" alt="arrow-right">',
     dots: false,
     responsive: [
       {
