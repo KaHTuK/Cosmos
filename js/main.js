3b7473c33d02b78d4bdd6889dab54e1b51bc38c3
KaHTuK <88055736+KaHTuK@users.noreply.github.com>
2021-08-27 17:36:34 +0300

    Add files via upload
    Добавил пагинацию в слайдер

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function () {
       '<img class="slider_arrow slider_arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
     nextArrow:
       '<img class="slider_arrow slider_arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          dots: true,
+        },
+      },
+    ],
   });
 
   $(".menu-btn").on("click", function () {
